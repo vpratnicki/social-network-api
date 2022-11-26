f87c16cbd2f9c14d20f4fd17a9ed77011b8a92b4
vpratnicki <vanessa@pratnicki.net>
2022-11-26 15:12:25 -0500

    Chore: updated user routes and controller to POST and DELETE.

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -14,13 +14,13 @@ router.route('/')
 .get(getAllUsers)
 .post(addUser);
 
-// Set up Get, Put and delete by id at /api/users/:id
+// Set up Get, Put and Delete by id at /api/users/:id
 router.route('/:id')
 .get(getUserById)
 .put(updateUser)
 .delete(deleteUser);
 
-// set POST and DELETE by user id /api/users/:userId/friends/:friendId
+// set up POST and DELETE at /api/users/:userId/friends/:friendId
 router.route('/api/users/:userId/friends/:friendId')
 .post(addFriend)
 .delete(deleteFriend); 
